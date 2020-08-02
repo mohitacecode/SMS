4e99aef2244d70b32e895b2c6985a19a8b87a308
akash mishra <amaparichit98@gmail.com>
2020-08-02 18:23:45 +0800

    Added style

--- wallPage.php
+++ wallPage.php
@@ -1,7 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<style>
+/* Float four columns side by side */
+.column {
+  /* float: left; */
+  /* width: 25%;
+  padding: 0 10px; */
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  
+}
 
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+    align : center;
+    margin: auto;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+  border-radius : 20px;
+}
+
+div.row>div:nth-of-type(odd) {
+  background: #ebebe0;
+}
+div.row>div:nth-of-type(even) {
+  background: #b8b894;
+}
+/* .card:nth-of-type(odd) {
+    background: #ebebe0;
+} */
+.card:nth-of-type(even) {
+    background:#b8b894;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+h1 {text-align:center;}
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 20px;
+}
+body {
+	background: linear-gradient(#141e30, #243b55);
+	
+}
+</style>
 </head>
 <body>
 <ul>
